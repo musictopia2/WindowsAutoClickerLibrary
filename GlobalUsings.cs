c768fb843e77d228ac53a8e3af94cc9058b7c8c3
Andy Walter <musictopia2@hotmail.com>
2024-01-12 19:44:21 -0700

    New class to force youtube to play.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using ww1 = System.Windows.Forms.Control;
 global using System.Runtime.InteropServices;
 global using CommonBasicLibraries.CollectionClasses;
+global using CommonBasicLibraries.MediaHelpers.Interfaces;
+global using aa1 = WindowsAutoClickerLibrary.AutoClickerHelpers;
---
+++ YouTubeForcePlayClass.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsAutoClickerLibrary;
+public class YouTubeForcePlayClass : IMediaForcePlay
+{
+    async Task IMediaForcePlay.ForcePlayAsync()
+    {
+        aa1.SetMousePosition(new(500, 500));
+        await Task.Delay(1000);
+        aa1.LeftClick();
+    }
+}
