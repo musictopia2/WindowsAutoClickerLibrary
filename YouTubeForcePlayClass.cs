5ca6d5379597ab742a7682ad9c502f0493ce0afc
Andy Walter <musictopia2@hotmail.com>
2025-11-12 16:55:19 -0700

    Fixed bug where i saw i did not need to wait to do left click anymore.

--- YouTubeForcePlayClass.cs
+++ YouTubeForcePlayClass.cs
@@ -1,10 +1,12 @@
 ﻿namespace WindowsAutoClickerLibrary;
 public class YouTubeForcePlayClass : IMediaForcePlay
 {
-    async Task IMediaForcePlay.ForcePlayAsync()
+    Task IMediaForcePlay.ForcePlayAsync()
     {
         aa1.SetMousePosition(new(500, 500));
-        await Task.Delay(3000); //try to wait longer so it can load.
-        aa1.LeftClick();
+        return Task.CompletedTask; //maybe not needed anymore (?)
+
+        //await Task.Delay(10000); //try to wait longer so it can load.
+        //aa1.LeftClick();
     }
 }
